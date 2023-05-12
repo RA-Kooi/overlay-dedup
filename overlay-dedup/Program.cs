4b874053e291469e1c5f11f9a9d707eef6fbbe1d
Rafaël Kooi <48814281+RA-Kooi@users.noreply.github.com>
2023-05-13 01:15:42 +0200

    Add initial code and license

---
+++ overlay-dedup/Program.cs
@@ -0,0 +1,19 @@
+// vim: set ff=unix nobomb ts=4 sw=4
+// LICENSE: GPLv3 (See LICENSE in the root of this repository)
+
+using System;
+
+namespace overlay_dedup;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("overlay-dedup  Copyright (C) 2023  Rafaël Kooi");
+		Console.WriteLine("This program comes with ABSOLUTELY NO WARRANTY;");
+		Console.WriteLine("This is free software, and you are welcome to "
+						  + "redistribute it under certain conditions;");
+		Console.WriteLine("See LICENSE or "
+						  + "https://www.gnu.org/licenses/gpl-3.0.html");
+	}
+}
